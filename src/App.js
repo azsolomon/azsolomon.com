831f227a3dd058377abf201664f21097947fa825
azsolomon <andrewzsolomon@gmail.com>
2019-01-28 21:45:16 +0100

    adding icons, changing font color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import pic from './photo.jpg';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 import './App.css';
 
 class App extends Component {
@@ -8,33 +12,43 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={pic} alt="pic" className="pic" />
-          <p>
+          <h1>
             Andrew Solomon
-          </p>
-          <a
-            className="App-link"
-            href="https://github.com/azsolomon"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>
-          <a
-            className="App-link"
-            href="https://www.linkedin.com/in/andrewzsolomon/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
-          <a
-            className="App-link"
-            href="mailto:andrewzsolomon@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Email
-          </a>
+          </h1>
+          <div className='icon-row'>
+              <a
+                className="App-link"
+                href="https://github.com/azsolomon"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub className='icon' size={40} />
+              </a>
+              <a
+                className="App-link"
+                href="https://www.linkedin.com/in/andrewzsolomon/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin className='icon' size={40} />
+              </a>
+              <a
+                className="App-link"
+                href="mailto:andrewzsolomon@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaEnvelope className='icon'size={40} />
+              </a>
+              <a
+                className="App-link"
+                href="https://www.instagram.com/andrew__solomon/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram className='icon' size={40} />
+              </a>
+          </div>
         </header>
       </div>
     );
